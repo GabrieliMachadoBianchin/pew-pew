495a56d32ef668c8ec69bed5d113e28a6108cf6a
Gabi <gbianchin@alunos.utfpr.edu.br>
2026-04-01 17:11:49 -0300

    add pew-pew

---
+++ Semana4/ProjetoDois/Assets/Scripts/BulletController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+        float randomx = UnityEngine.Random.Range(-15, 15);
+        float randomz = UnityEngine.Random.Range(-15, 15);
+
+        GameObject enemy = (GameObject)Instantiate(Resources.Load("Enemy", typeof(GameObject)));
+
+        enemy.transform.position = new Vector3(randomx, 0, randomz );
+
+        enemy.transform.rotation = Quaternion.Euler(0, UnityEngine.Random.Range(0, 360), 0);
+
+        Destroy(gameObject);
+    }
+}
---
+++ Semana4/ProjetoDois/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime*150.0f ;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime*3.0f ;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space)){
+
+            Fire();
+        }
+
+    }
+
+    void Fire()
+    {
+        var bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+        bullet.GetComponent<Rigidbody>().linearVelocity = bullet.transform.forward * 6f ;
+        Destroy(bullet.transform, 3f);
+    }
+}
